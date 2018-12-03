63983b59fdf5da0deddf2814bb5c58eeb51344de
sushmabn <32410715+sushmabn@users.noreply.github.com>
2018-12-03 14:20:14 +0530

    Update out.py

--- out.py
+++ out.py
@@ -1,8 +1,8 @@
 def console_logs(Finished):
     print "entry to code"
-    if(string(Finished) == "SUCCESS")
+    if(string(Finished) == "SUCCESS"):
         print 'Build completed successfully!'
-    else
+    else:
         print 'Build completed with failure'
 
 if argv[1]=='console_logs':
