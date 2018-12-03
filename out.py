c93fec2b2fdc36fae634e655f0f03bc9f694a6f4
sushmabn <32410715+sushmabn@users.noreply.github.com>
2018-12-03 13:11:06 +0530

    new py file

---
+++ out.py
@@ -0,0 +1,10 @@
+def console_logs(Finished):
+    print "entry to code"
+    if(Finished == "SUCCESS")
+        print 'Build completed successfully!'
+    else
+        print 'Build completed with failure'
+
+if argv[1]=='console_logs':
+    
+    z=console_logs(argv[2])
